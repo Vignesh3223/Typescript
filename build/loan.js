2a18e8756f1366e5967a30b52b04ac2aa39881ca
Vignesh3223 <viketvicky@gmail.com>
2023-03-21 16:08:52 +0530

    commit1

---
+++ build/loan.js
@@ -0,0 +1,34 @@
+"use strict";
+class Customer {
+    constructor(name, age, address, contact) {
+        this.Name = name;
+        this.Age = age;
+        this.Address = address;
+        this.Conatct = contact;
+    }
+    display() {
+        console.log("Customer Name : " + this.Name + "\n" + "Age : " + this.Age + "\n" + "Address : " + this.Address + "\n" + "Contact Number : " + this.Conatct + "\n");
+    }
+}
+class Loan extends Customer {
+    constructor(name, age, address, contact, accno, ltype, lamt, dur) {
+        super(name, age, address, contact);
+        this.loanaccountno = accno;
+        this.loantype = ltype;
+        this.loanamt = lamt;
+        this.duration = dur;
+    }
+    interestcalculate(interest, total) {
+        interest = this.loanamt / this.duration;
+        total = this.loanamt + interest;
+    }
+    display() {
+        super.display();
+        console.log("Account Number : " + this.loanaccountno + "\n" + "Loan type : " + this.loantype + "\n" + "Loan Amount : " + this.loanamt + "\n" +
+            "Duration : " + this.duration + "\n");
+        console.log(`Interest to be Paid :  + $(interest)`);
+        console.log(`Total Amount to be Paid :  + $(total)`);
+    }
+}
+let loan = new Loan('Rajesh', 32, '4B,Meco Gardens,Kanuvai', 9366052147, 12115, 'Personal', 200000, 24);
+loan.display();
